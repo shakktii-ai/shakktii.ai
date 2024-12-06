5b07cff00ec750db0f2812837c042bc74750ae44
shakktii.ai <shakktii.ai@gmail.com>
2024-12-06 16:41:47 +0530

    first commit

--- components/maps.js
+++ components/maps.js
@@ -8,10 +8,43 @@ const MAPS = () => {
   useEffect(() => {
     const getLocations = async () => {
       const fakeLocation = [
-        { title: 'महाड', lat: 15.9182808, lng: 108.3470323, description: 'this is Location A' },
-        { title: 'फलटण', lat: 16.0471659, lng: 108.1716864, description: 'this is Location B' },
-        { title: 'पाचोरा', lat: 20.8467333, lng: 106.6637271, description: 'this is Location C' },
-        { title: ' चिंचवड', lat: 10.823099, lng: 106.629664, description: 'this is Location D' },
+        { title: 'महाड', lat:  18.08209397458222, lng: 73.42234238155838, description: `Ground intellegance, End To End Social Media 
+Management, Social Media Management 
+Team, Project Manager, Broadcasting Tools, 
+Audio Vidio Bridge, Auto Posting, Auto 
+Commenting` },
+        { title: 'फलटण', lat: 17.993249003656196, lng: 74.42942449739226, description: `Ground intellegance, End To End Social Media 
+Management, Social Media Management 
+Team, Project Manager, Broadcasting Tools, 
+Audio Vidio Bridge, Auto Posting, Auto 
+Commenting` },
+        { title: 'Jamner', lat: 20.809693513918464, lng:  75.77802942639441, description: ` Ground intellegance, End To End Social Media 
+Management, Co-ordinator Manager, 
+Broadcasting Tools, Audio Vidio Bridge, Auto 
+Posting, Auto Commenting, All AI Tools.` },
+        { title: '  Pachora', lat:  20.65919095233716, lng: 75.3478719850142, description: `Ground intellegance, End To End Social Media 
+Management, Project Manager, Broadcasting 
+Tools, Audio Vidio Bridge, Auto Posting, Auto 
+Commenting` },
+        { title: ' Nagar', lat:  19.09051436028614, lng: 74.74678713274912, description: ` Ground intellegance, End To End Social Media 
+Management, Social Media Management 
+Team, Project Manager, Broadcasting Tools, 
+Audio Vidio Bridge, Auto Posting, Auto 
+Commenting, Trolling Contain, 
+Taglines/Hashtags, daily Cratives.` },
+        { title: '  Chinchwad', lat: 18.639050644225645, lng: 73.79385485804474, description: ` Ground intellegance, End To End Social Media 
+Management, Social Media Management 
+Team, Project Manager, Broadcasting Tools, 
+Audio Vidio Bridge, Auto Posting, Auto 
+Commenting, Wisper campaining.` },
+        { title: ' Bramhapuri', lat: 20.611129583843827, lng: 79.85732078839634, description: `Ground intellegance, End To End Social Media 
+Management,  Broadcasting Tools, Audio Vidio 
+Bridge, Auto Posting, Auto Commenting, daily 
+Cratives` },
+        { title: ' Mahim', lat: 19.04080586011608, lng: 72.8442694173768, description: ` Ground intellegance, End To End Social Media 
+Management,  Broadcasting Tools, Audio Vidio 
+Bridge, Auto Posting, Auto Commenting, daily 
+Cratives` },
       ];
       setLocations(fakeLocation);
     };
@@ -27,8 +60,8 @@ const MAPS = () => {
             width: '100%',
             height: '100%',
           }}
-          zoom={16}
-          center={{ lat: 15.9182808, lng: 108.3470323 }}
+          zoom={5}
+          center={{ lat: 22.788991848868555, lng: 79.30530240916862 }}
           options={{
             zoomControl: true,
             streetViewControl: true,
